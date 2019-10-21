384cf1e8cfc91d306d424180e44731373b555897
Kensen <hirohtk@gmail.com>
2019-10-20 22:31:35 -0700

    EOD 2019_1020 - added modal to fire when the original link button (project name) was clicked

---
+++ app.js
@@ -0,0 +1,48 @@
+var modal = document.getElementById("projectModal");
+
+$("#thumbnailBox6").on("click", function (){
+    console.log("box6 clicked");
+    modal.style.display = "block";
+    disableScroll();
+});
+
+// Get the button that opens the modal
+//var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+//btn.onclick = function() {
+//  modal.style.display = "block";
+//}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  enableScroll();
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    enableScroll();
+  }
+}
+
+
+function disableScroll() { 
+    // Get the current page scroll position 
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop; 
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft, 
+  
+        // if any scroll is attempted, set this to the previous value 
+        window.onscroll = function() { 
+            window.scrollTo(scrollLeft, scrollTop); 
+        }; 
+} 
+  
+function enableScroll() { 
+    window.onscroll = function() {}; 
+} 
