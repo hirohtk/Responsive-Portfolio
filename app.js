7302a47a7d2625b69b3ddf0af95d8dbebde2120d
Kensen <hirohtk@gmail.com>
2019-10-21 18:31:48 -0700

    something very wrong with section between 768 and 640px that allows links not to work.

--- app.js
+++ app.js
@@ -1,48 +1,62 @@
-var modal = document.getElementById("projectModal");
-
-$("#thumbnailBox6").on("click", function (){
-    console.log("box6 clicked");
-    modal.style.display = "block";
-    disableScroll();
-});
-
-// Get the button that opens the modal
-//var btn = document.getElementById("myBtn");
+var modalPr1 = document.getElementById("project1Modal");
+var modalAss1 = document.getElementById("Ass1Modal");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-//btn.onclick = function() {
-//  modal.style.display = "block";
-//}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-  enableScroll();
-}
+$("#thumbnailBox1").on("click", function () {
+   
+    modalAss1.style.display = "block";
+    disableScroll();
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-    enableScroll();
-  }
-}
+    span.onclick = function () {
+        modalAss1.style.display = "none";
+        enableScroll();
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modalAss1) {
+            modalAss1.style.display = "none";
+            enableScroll();
+        }
+    }
+});
 
 
-function disableScroll() { 
+$("#thumbnailBox6").on("click", function () {
+    modalPr1.style.display = "block";
+    disableScroll();
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modalPr1.style.display = "none";
+        enableScroll();
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modalPr1) {
+            modalPr1.style.display = "none";
+            enableScroll();
+        }
+    }
+});
+
+
+
+function disableScroll() {
     // Get the current page scroll position 
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop; 
-    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft, 
-  
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+
         // if any scroll is attempted, set this to the previous value 
-        window.onscroll = function() { 
-            window.scrollTo(scrollLeft, scrollTop); 
-        }; 
-} 
-  
-function enableScroll() { 
-    window.onscroll = function() {}; 
+        window.onscroll = function () {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+}
+
+function enableScroll() {
+    window.onscroll = function () { };
 } 
